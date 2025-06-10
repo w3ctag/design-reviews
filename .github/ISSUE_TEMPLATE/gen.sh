49f305a6ae5ac000339c4cc4f42adba170577844
Jeffrey Yasskin <jyasskin@google.com>
2025-06-10 00:56:12 +0000

    Fix code review comments.

--- .github/ISSUE_TEMPLATE/gen.sh
+++ .github/ISSUE_TEMPLATE/gen.sh
@@ -5,4 +5,4 @@ NPX=${NPX:-npx}
 $NPX ejs intake.yaml.ejs -i '{"type": "incubation"}' -o 000-incubation-review.yaml
 $NPX ejs intake.yaml.ejs -i '{"type": "wg-new"}' -o 005-wg-new-spec-review.yaml
 $NPX ejs intake.yaml.ejs -i '{"type": "wg-revision"}' -o 010-wg-revision-review.yaml
-$NPX ejs intake.yaml.ejs -i '{"type": "browser"}' -o 015-browser-review.yaml
+$NPX ejs intake.yaml.ejs -i '{"type": "other"}' -o 015-other-spec-review.yaml
